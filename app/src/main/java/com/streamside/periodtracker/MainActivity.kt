9bd55942ae5c5acd6d529867612ef241f6ad0c6c
dizonivan11 <dizonivan11@gmail.com>
2023-10-05 21:06:27 +0800

    Added First Time entry logic

--- app/src/main/java/com/streamside/periodtracker/MainActivity.kt
+++ app/src/main/java/com/streamside/periodtracker/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
 import com.streamside.periodtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,20 +17,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val firstTime : Boolean = preferences.getBoolean(getString(R.string.first_time_key), true)
 
-        val navView: BottomNavigationView = binding.navView
+        if (firstTime) {
+            // TODO: First time setup wizard viewpager
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_settings
+            // Set First Time settings to false
+            preferences.edit().putBoolean(getString(R.string.first_time_key), false).apply()
+        } else {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            val navView: BottomNavigationView = binding.navView
+
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_settings
+                )
             )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
+        }
     }
 }
