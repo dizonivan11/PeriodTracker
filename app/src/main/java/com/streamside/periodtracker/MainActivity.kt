48f54a44ab3a25be09bef083ae11bc5b06b61c5d
dizonivan11 <dizonivan11@gmail.com>
2023-10-06 22:05:01 +0800

    Initialized Setup Wizard

--- app/src/main/java/com/streamside/periodtracker/MainActivity.kt
+++ app/src/main/java/com/streamside/periodtracker/MainActivity.kt
@@ -3,34 +3,43 @@ package com.streamside.periodtracker
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.streamside.periodtracker.databinding.ActivityMainBinding
+import com.streamside.periodtracker.setup.IntroFragment
+
+private const val SETUP_PAGES = 1
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         val preferences = PreferenceManager.getDefaultSharedPreferences(this)
         val firstTime : Boolean = preferences.getBoolean(getString(R.string.first_time_key), true)
 
         if (firstTime) {
             // TODO: First time setup wizard viewpager
+            setContentView(R.layout.activity_setup)
+
+            val viewPager : ViewPager2 = findViewById(R.id.setup_vp)
+            val pagerAdapter = SetupPagerAdapter(this)
+            viewPager.adapter = pagerAdapter
 
             // Set First Time settings to false
-            preferences.edit().putBoolean(getString(R.string.first_time_key), false).apply()
+            // preferences.edit().putBoolean(getString(R.string.first_time_key), false).apply()
         } else {
-            binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
-
             val navView: BottomNavigationView = binding.navView
-
             val navController = findNavController(R.id.nav_host_fragment_activity_main)
             // Passing each menu ID as a set of Ids because each
             // menu should be considered as top level destinations.
@@ -42,5 +51,20 @@ class MainActivity : AppCompatActivity() {
             setupActionBarWithNavController(navController, appBarConfiguration)
             navView.setupWithNavController(navController)
         }
+        supportActionBar?.hide()
+    }
+
+    inner class SetupPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int {
+            return SETUP_PAGES
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> IntroFragment()
+                else -> IntroFragment()
+            }
+        }
+
     }
 }
