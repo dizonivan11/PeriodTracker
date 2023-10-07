08a7ff6500d44d30d37209eefacab880df5ed457
dizonivan11 <dizonivan11@gmail.com>
2023-10-08 02:43:22 +0800

    Revised color resources and initialized dark mode implementation

--- app/src/main/java/com/streamside/periodtracker/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/streamside/periodtracker/ui/settings/SettingsFragment.kt
@@ -1,12 +1,20 @@
 package com.streamside.periodtracker.ui.settings
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.streamside.periodtracker.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        findPreference<SwitchPreference>(getString(R.string.dark_mode_key))?.setOnPreferenceChangeListener { _, newValue ->
+            if (newValue as Boolean) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            true
+        }
     }
 }
